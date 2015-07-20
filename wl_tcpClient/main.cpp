e35c08dfbcd192e88b3e0ae6322c5bb77fab0238
gessd <346962839@qq.com>
2015-07-20 22:02:29 +0800

    20-22

---
+++ wl_tcpClient/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <QtGui/QApplication>
+#include "client.h"
+
+#include <QTextCodec>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+//    QTextCodec::setCodecForTr(QTextCodec::codecForLocale());//添加中文支持2-2
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+//    Widget w;
+//    w.show();
+    client w1;
+    w1.show();
+
+    return a.exec();
+}
